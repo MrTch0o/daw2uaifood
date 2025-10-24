50d83514b59c3aadb0631a170b12bcd50c757c70
MrTch0o <waguinhotcho@Gmail.com>
2025-10-24 17:29:38 -0300

    Fix: CreateUserDto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove campos não declarados no DTO
+      forbidNonWhitelisted: true, // lança erro se enviar campos a mais
+      transform: true, // transforma os tipos automaticamente
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error('Erro ao iniciar a aplicação:', err);
+  process.exit(1);
+});
+
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,8 +1,29 @@
 import { UserType } from '@prisma/client';
+import {
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
 
 export class CreateUserDto {
+  @IsString()
   name: string;
+
+  @IsString()
+  @IsPhoneNumber('BR', { message: 'Número de telefone inválido' })
   phone: string;
+
+  @IsString()
+  @IsStrongPassword(
+    { minLength: 8 },
+    {
+      message:
+        'Senha fraca. Deve conter pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula, um número e um caractere especial',
+    },
+  )
   password?: string; // Senha é opcional aqui, mas obrigatória na lógica
+
+  @IsOptional()
   type?: UserType;
 }
